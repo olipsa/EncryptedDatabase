b86f0f36685c11e18653106d54eb2209ef98eae4
Ondina <ondina_patricia@yahoo.com>
2022-01-10 13:37:58 +0200

    Implement add command.

---
+++ cmd/__init__.py
@@ -0,0 +1,5 @@
+"""
+Contains modules for handling commands received from end-user.\n
+Support for new commands has to be added in listener module,
+and they must be implemented in commands module.
+"""
--- cmd/commands.py
+++ cmd/commands.py
@@ -1,34 +1,66 @@
+"""Actual implementation of supported commands. """
+import os.path
+from database.connection import find_file
+from security.encryption import Algorithms
+
+alg_class = Algorithms()
+
+
 class Add:
-    def __init__(self, folder):
+    def __init__(self):
         self.file_path = None
-        self.encrypted_folder = folder
+        self.input_alg = None
 
     def start(self):
+        self._input_path()
+        if self.file_path is not None:
+            self._input_algorithm()
+
+    def _input_path(self):
         while True:
             path = input("Provide the path of the file to be added or 'none' to stop command: ")
             if path.lower() == "none":
-                break
-            try:
-                with open(path, "rb") as f:
-                    content = f.read()
-                    self.file_path = path
-                    break
-            except FileNotFoundError:
+                return False
+            elif not os.path.exists(path):
                 print("Incorrect path.")
-            except PermissionError:
-                print("No permission to open this file.")
+            elif os.path.isdir(path):
+                print("Unable to encrypt entire directory.")
+            elif os.path.exists(alg_class.ENCRYPTED_FOLDER+os.path.basename(path)+"_enc")\
+                    or find_file(os.path.basename(path)):
+                print("File with same name already encrypted.")
+            else:
+                self.file_path = path
+                return True
+
+    def _input_algorithm(self):
+        while self.input_alg is None:
+            for i in range(len(alg_class.ENCRYPTION_ALG)):
+                print(str(i+1)+". "+alg_class.ENCRYPTION_ALG[i])
+
+            alg_index = input("Choose the encryption algorithm to be used: ")
+
+            try:
+                alg_index = int(alg_index)
+            except ValueError:
+                print("Invalid number provided.")
+                continue
+            if alg_index < 1 or alg_index > len(alg_class.ENCRYPTION_ALG):
+                print("Invalid number provided.")
+                continue
+            self.input_alg = alg_index - 1
 
     def treat(self):
-        if self.file_path is None:
+        if self.file_path is None or self.input_alg is None:
             print("No action will be taken.")
         else:
-            print("Adding file to db..")
+            print("Encrypting file...")
+            alg_class.encrypt_file(self.file_path, self.input_alg)
+
 
 
 class Read:
-    def __init__(self, folder):
+    def __init__(self):
         self.file_name = None
-        self.encrypted_folder = folder
 
     def start(self):
         while True:
@@ -36,10 +68,9 @@ class Read:
             if file.lower() == "none":
                 break
             try:
-                f = open(self.encrypted_folder+file, "rb")
+                f = open(alg_class.ENCRYPTED_FOLDER + file, "rb")
             except FileNotFoundError:
                 print("File not found.")
-                print("../encrypted_files/"+self.file_name)
             else:
                 self.file_name = file
                 break
@@ -52,9 +83,8 @@ class Read:
 
 
 class Delete:
-    def __init__(self, folder):
+    def __init__(self):
         self.file_name = None
-        self.encrypted_folder = folder
 
     def start(self):
         while True:
@@ -62,7 +92,7 @@ class Delete:
             if file == "none":
                 break
             try:
-                f = open(self.encrypted_folder + file, "rb")
+                f = open(alg_class.ENCRYPTED_FOLDER + file, "rb")
             except FileNotFoundError:
                 print("File not found.")
             else:
