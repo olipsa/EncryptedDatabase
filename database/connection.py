04b3c1fbe1e687d02df6a9e29ae4d47fed53ff24
Ondina <ondina_patricia@yahoo.com>
2022-01-10 19:37:39 +0200

    Added delete method for removing file from db.

--- database/connection.py
+++ database/connection.py
@@ -3,7 +3,6 @@ import sqlite3
 
 
 con = None
-cursor = None
 
 
 def connect(db_path):
@@ -12,11 +11,12 @@ def connect(db_path):
 
 
 def close_connection():
+    global con
     con.close()
 
 
 def create_table():
-    global con, cursor
+    global con
     cursor = con.cursor()
     cursor.execute('''CREATE TABLE IF NOT EXISTS enc_files
                            (file_name text NOT NULL PRIMARY KEY, 
@@ -27,7 +27,7 @@ def create_table():
 
 
 def insert_file(file_name, algorithm, public_key, private_key):
-    global con, cursor
+    global con
     cursor = con.cursor()
     try:
         cursor.execute(f"INSERT INTO enc_files VALUES('{file_name}','{algorithm}', '{public_key}','{private_key}') ")
@@ -35,13 +35,14 @@ def insert_file(file_name, algorithm, public_key, private_key):
         print(e)
         return False
     else:
+        con.commit()
         return True
     finally:
         cursor.close()
 
 
 def find_file(file_name):
-    global con, cursor
+    global con
     cursor = con.cursor()
     result = cursor.execute(f"SELECT * FROM enc_files WHERE file_name='{file_name}'")
     if result.fetchone() is not None:
@@ -49,6 +50,18 @@ def find_file(file_name):
         return True
     cursor.close()
     return False
+
+
+def delete_file(file_name):
+    global con
+    cursor = con.cursor()
+    result = cursor.execute(f"DELETE FROM enc_files WHERE file_name='{file_name}'")
+    if result.rowcount > 0:
+        print("File removed from db.")
+        con.commit()
+    else:
+        print("File not stored in db.")
+    cursor.close()
 
 
 
